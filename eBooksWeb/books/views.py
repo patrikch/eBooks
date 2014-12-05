a8577a5e80aea356b996bf7a5af04987f6314531
Patrik Chytil <patrikch@centrum.cz>
2014-12-05 15:35:54 +0100

    changes in templates

--- eBooksWeb/books/views.py
+++ eBooksWeb/books/views.py
@@ -3,5 +3,18 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,"home.html")
+
+def list(request):
+    return render(request,"list.html")
+
+def search(request):
+    return render(request,"advSearch.html")
+
+def keysearch(request):
+    return render(request,"keySearch.html")
+
+def detail(request,id):
+    return render(request,"detail.html")
+
     
 
--- eBooksWeb/eBooksWeb/urls.py
+++ eBooksWeb/eBooksWeb/urls.py
@@ -9,13 +9,14 @@ site_media = os.path.join(os.path.dirname(__file__),
 urlpatterns = patterns('',    
     #session management
     url(r"^login/$","django.contrib.auth.views.login",name="login"),
-    url(r"^logout/$",logout,name="logout"),                       
+    url(r"^logout/$",'django.contrib.auth.views.logout',name="logout"),                       
     # books app
     url(r'^$', 'books.views.home', name='home'),
+    url(r'^home/$', 'books.views.home', name='home'),
     url(r'^detail/(\d+)/$', 'books.views.detail', name='detail'),
-    url(r'^search$', 'books.views.search', name='search'),
-    url(r'^keysearch$', 'books.views.keysearch', name='keysearch'),
-    url(r'^list$', 'books.views.list', name='list'),
+    url(r'^search/$', 'books.views.search', name='search'),
+    url(r'^keysearch/$', 'books.views.keysearch', name='keysearch'),
+    url(r'^list/$', 'books.views.list', name='list'),
     #site media
     url(r"^site_media/(?P<path>.*)$","django.views.static.serve",
          {"document_root":site_media}),
